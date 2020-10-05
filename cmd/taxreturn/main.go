e7a0fc36908658043e63a483f58025c34c47f47d
antklim <antklim@gmail.com>
2020-10-05 17:12:47 +1100

    Reading csv added

--- cmd/taxreturn/main.go
+++ cmd/taxreturn/main.go
@@ -1,13 +1,47 @@
 package main
 
-import "github.com/spf13/cobra"
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
 
-var rootCmd = &cobra.Command{
-	Use:   "taxreturn",
-	Short: "taxreturn - tax and expenses calculator.",
-	Long:  "",
+	"github.com/spf13/cobra"
+)
+
+var (
+	recordsFile string // path to the file with the bills records
+
+	rootCmd = &cobra.Command{
+		Use:   "taxreturn",
+		Short: "taxreturn - tax and expenses calculator.",
+		Long:  "",
+		RunE:  taxreturn,
+	}
+)
+
+func init() {
+	rootCmd.Flags().StringVarP(&recordsFile, "records", "r", "", "Path to the bills records file (required)")
+	rootCmd.MarkFlagRequired("records")
+}
+
+func taxreturn(ccmd *cobra.Command, args []string) error {
+	recordFile, err := os.Open(recordsFile)
+	if err != nil {
+		return err
+	}
+
+	reader := csv.NewReader(recordFile)
+	records, err := reader.ReadAll()
+
+	fmt.Println(records)
+
+	return nil
 }
 
 func main() {
-	rootCmd.Execute()
+	err := rootCmd.Execute()
+	if err != nil {
+		fmt.Println("Can't execute command:", err)
+		os.Exit(1)
+	}
 }
